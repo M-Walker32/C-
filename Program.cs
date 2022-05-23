91e8062591fe67c6100911bb80e20b7f7846f8cf
Maria Walker <mariawalker3232@gmail.com>
2022-05-23 14:44:49 -0600

    initial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+bool playing = true;
+System.Console.WriteLine(playing);
+
+while (playing)
+{
+  string keepPlaying = System.Console.ReadKey();
+  if (keepPlaying == n)
+  {
+    playing = false;
+  };
+  if (keepPlaying == y)
+  {
+    playing = true;
+  };
+}
