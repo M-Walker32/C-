7912b3e79801c69ed7f58af22f906ca2f8a83005
Maria Walker <mariawalker3232@gmail.com>
2022-05-23 19:11:32 -0600

    done

--- Program.cs
+++ Program.cs
@@ -1,17 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 bool playing = true;
-System.Console.WriteLine(playing);
+Random rnd = new Random();
+int num = rnd.Next(1, 3);
+string computer = "computers choice";
+
+// Get users name
+System.Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
 
 while (playing)
 {
-  string keepPlaying = System.Console.ReadKey();
-  if (keepPlaying == n)
+  // Ask to play game
+  System.Console.WriteLine(name + ", do you want to play rock paper sissors? yes or no");
+  string play = Console.ReadLine().ToLower();
+  if (play == "no")
   {
     playing = false;
-  };
-  if (keepPlaying == y)
+  }
+  if (play == "yes")
   {
     playing = true;
+  }
+  else
+  {
+    Console.WriteLine("please enter yes or no");
+    Console.Clear();
   };
+  // User chooses rock paper sissors
+  System.Console.WriteLine("Pick a number 1. Rock 2. Paper, or 3. Sissors?");
+  string choice = Console.ReadLine().ToLower();
+  // GAME LOGIC
+  if (num == 1) { computer = "rock"; };
+  if (num == 2) { computer = "paper"; };
+  if (num == 3) { computer = "sissors"; };
+  if (choice == "1")
+  {
+    Console.WriteLine("The computer chooses " + computer);
+  }
+  if (choice == "2")
+  {
+    Console.WriteLine("The computer chooses " + computer);
+  }
+  if (choice == "3")
+  {
+    Console.WriteLine("The computer chooses " + computer);
+  }
+  else
+  {
+    Console.WriteLine("Please enter 1, 2, or 3");
+    Thread.Sleep(5000);
+    Console.Clear();
+  }
+
+  // System.Console.WriteLine("Would you like to continue playing?");
+  // if (play == "no")
+  // {
+  //   playing = false;
+  //   Console.Clear();
+  // }
+  // if (play == "yes")
+  // {
+  //   playing = true;
+  // }
+  // else
+  // {
+  //   Console.WriteLine("please enter yes or no");
+  // };
 }
